9d4d4bee8abfc804db3bc9b95112f49bfd408f46
AngelGallegosE <7angelgallegos@gmail.com>
2023-07-25 18:35:47 -0600

    initial commit

---
+++ js/RyOSnUjftyvs.js
@@ -0,0 +1,20 @@
+function mostrar(){
+	document.getElementById('datos').style.display="block";
+	
+}
+
+function mostrar2(){
+
+	document.getElementById('wedding-sectioncam').style.display="none";
+	document.getElementById('datos2').style.display="block";
+	
+}
+
+
+
+function mostrar3(){
+	document.getElementById('datos2').style.display="none";
+
+	document.getElementById('wedding-sectioncam').style.display="block";
+
+}
